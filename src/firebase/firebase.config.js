f1556504158495c628c2f1c7a7560b08ad738448
Kakon Debnath <kakondebnath96@gmail.com>
2023-05-01 08:43:48 +0600

    add secract env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB56wF-pOopampNH85pTFhWYO0CefAE5kc",
-  authDomain: "simple-firebase-5161f.firebaseapp.com",
-  projectId: "simple-firebase-5161f",
-  storageBucket: "simple-firebase-5161f.appspot.com",
-  messagingSenderId: "1076028096811",
-  appId: "1:1076028096811:web:9d08ca295ed5181475c56e"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
